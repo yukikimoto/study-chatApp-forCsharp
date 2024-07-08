dae7153b32004b49505f92e2c300bd70c6549f4a
yukikimoto <KTC2243020002@edu.kyoto-tech.ac.jp>
2024-07-08 19:44:20 +0900

    環境変数以外のコードの実装

--- chatApp/Program.cs
+++ chatApp/Program.cs
@@ -1,15 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using DotNetEnv;
+using Microsoft.SemanticKernel;
 
-Console.WriteLine("Hello, World!");
 
 // .envファイルをロード
 Env.Load();
 
 // .envファイルの内容を取得
 string apiKey = Env.GetString("API_KEY");
-string testData = Env.GetString("TEST_DATA");
 
-Console.WriteLine("apikey : " + apiKey);
-Console.WriteLine("test data : " + testData);
+
+// カーネルを作成
+var builder = Kernel.CreateBuilder();
+
+// Azure OpenAI Chat Completion を追加
+builder.AddAzureOpenAIChatCompletion(
+    "<<gpt 3.5 turbo のモデルのデプロイ名>>",
+    "https://<<Azure OpenAI Service のリソース名>>.openai.azure.com/",
+    "<<Azure OpenAI Service の API キー>>");
+
+// カーネルをビルド
+var kernel = builder.Build();
+
+
+// プロンプトを送信して返答を受け取る
+var result = await kernel.InvokePromptAsync(
+    """
+    Hello, world! と表示する C# のプログラムを書いてください。
+    """);
+
+Console.WriteLine(result.GetValue<string>());
